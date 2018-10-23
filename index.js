9f49b763d266bf1241acd20e75bcc743a984509c
Brian Chen <brian.chen@tabcorp.com.au>
2018-10-23 11:15:44 +1100

    make hash use md5

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
 const uniqid = require('uniqid');
+const md5 = require('md5');
 const appRootPath = require('app-root-path');
 
 module.exports = (workingDir, patterns) => new Promise(resolve => {
@@ -24,7 +25,7 @@ module.exports = (workingDir, patterns) => new Promise(resolve => {
 
   // Replace file names with hashed counterpart and store reference of hashed name
   const hashedFiles = getSimplePath(hashingFiles.map(fileName => {
-    const hash = uniqid();
+    const hash = md5(uniqid());
 
     const array = fileName.split('/');
 
